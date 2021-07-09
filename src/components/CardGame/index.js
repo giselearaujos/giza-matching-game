55f399909c6003611fa3366ceb3908f6d2ed34d5
giselearaujos <giselearaujo07.s@gmail.com>
2021-07-09 10:30:26 -0300

    :tada: Create CardGame component

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,12 @@
+import "./style.css";
+
+function CardGame() {
+    return `
+        <article class="card-game">
+            <img src="images/giza.png" alt="Caricatura da Giza" />
+        </article>
+    
+    `;
+}
+
+export default CardGame;
